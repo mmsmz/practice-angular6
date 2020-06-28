95e672d90bfe37c0f96422854f00fc53f22b709a
Mohamed Arafath <35114431+mmsmz@users.noreply.github.com>
2020-06-28 21:43:27 +0530

    Added the files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent 
+    AppComponent
   ],
   imports: [
     BrowserModule,
